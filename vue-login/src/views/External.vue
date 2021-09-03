e132d434e0fe886aa43187383b7fc169056390cd
ti-gi <42831649+ti-gi@users.noreply.github.com>
2021-09-03 10:51:19 +0200

    Add files via upload

---
+++ vue-login/src/views/External.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="callApi">Call</button>
+    <p>{{ apiMessage }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "external",
+  data() {
+    return {
+      apiMessage: ""
+    };
+  },
+  methods: {
+    async callApi() {
+      // Get the access token from the auth wrapper
+      const token = await this.$auth.getTokenSilently();
+
+      // Use Axios to make a call to the API
+      const { data } = await axios.get("https://localhost:44367/api/private", {
+        headers: {
+          Authorization: `Bearer ${token}`    // send the access token through the 'Authorization' header
+        }
+      });
+
+      this.apiMessage = data;
+    }
+  }
+};
+</script>
